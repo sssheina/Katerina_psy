310b5bb97cb722dc65a0db89ccf70e84d898e4f5
Olga Bulgakova <108359930+OlgaBuLL@users.noreply.github.com>
2024-09-17 16:47:35 +0200

    methods localization (not completed)

--- assets/scripts/methods.js
+++ assets/scripts/methods.js
@@ -1,4 +1,4 @@
-const methodsData = [
+const methodsDataRu = [
   {
     title: "Секс-терапия",
     text: `Терапия эмоциональных и психологических аспектов сексуальных проблем. <br> Помогает решить проблемы, связанные с интимностью`,
@@ -13,7 +13,7 @@ const methodsData = [
   },
   {
     image: "/assets/images/method_pic-card_1.png",
-    alt: "абстрактные линии",
+    alt: "Абстрактные линии",
   },
   {
     title: "IFS при травме насилия",
@@ -24,6 +24,78 @@ const methodsData = [
     text: `Метод использует творчество для работы с бессознательным, помогает выразить и осознать свои чувства через искусство`,
   },
 ];
+
+const methodsDataEn = [
+  {
+    title: "Sex-therapy",
+    text: `Therapy for emotional and psychological aspects of sexual problems. Helps to solve issues related to intimacy and sexual relationships`,
+  },
+  {
+    title: "АСТ",
+    text: `The method helps you to stay in touch with uncomfortable feelings and thoughts, make informed decisions based on your values`,
+  },
+  {
+    title: "EFT",
+    text: `The method works with emotions and behaviour, helps to establish communication between partners, reveal true needs`,
+  },
+  {
+    image: "/assets/images/method_pic-card_1.png",
+    alt: "Abstract lines",
+  },
+  {
+    title: "IFS for trauma violence",
+    text: `The Internal Family Systems model is used for recovery from experiences of violence and abusive relationships. Works with abusers as well`,
+  },
+  {
+    title: "Art therapy",
+    text: `The method uses creativity to reveal processes in the unconscious, helps to identify and express your feelings through art`,
+  },
+];
+
+const methodsDataFr = [
+  {
+    title: "Sexothérapie",
+    text: `Cette thérapie est dédiée aux aspects émotionnels et psychologiques des problèmes sexuels. Elle aide à résoudre les difficultés liés à l’intimité`,
+  },
+  {
+    title: "ACT",
+    text: `La méthode vous aide à rester en contact avec des sentiments et des pensées inconfortables, prendre des décisions éclairées basées sur vos valeurs`,
+  },
+  {
+    title: "EFT",
+    text: `La méthode adresse des réactions émotionnelles et le comportement, aide à établir la communication en union en révélant de vrais besoins`,
+  },
+  {
+    image: "/assets/images/method_pic-card_1.png",
+    alt: "Lignes abstraites",
+  },
+  {
+    title: "IFS pour traumatismes",
+    text: `Le modèle de systèmes familiaux internes (IFS) traite les conséquences de violence et de relations abusives. Inclu le travail avec des acteur.ices de violance`,
+  },
+  {
+    title: "Art-thérapie",
+    text: `La méthode utilise la créativité pour travailler avec l'inconscient, aide à exprimer et réalisez vos sentiments à travers des techniques artistiques`,
+  },
+];
+
+const methodsDataMap = {
+  RU: {
+    title: "Я применяю эти методы",
+    data: methodsDataRu,
+    buttonText: "Запись",
+  },
+  EN: {
+    title: "I use these methods",
+    data: methodsDataEn,
+    buttonText: "Start First session",
+  },
+  FR: {
+    title: "J’utilise ces méthodes",
+    data: methodsDataFr,
+    buttonText: "Prendre RDV",
+  },
+};
 
 const handleGenerateMethodCard = ({ title, text, image, alt }) => {
   if (image) {
@@ -41,5 +113,7 @@ const handleGenerateMethodCard = ({ title, text, image, alt }) => {
     `;
 };
 
-const methodsContainer = document.getElementById("methodsCards");
-methodsContainer.innerHTML = methodsData.map(handleGenerateMethodCard).join("");
+const methodsContainer = document.querySelector(".methods__cards");
+methodsContainer.innerHTML = methodsDataRu
+  .map(handleGenerateMethodCard)
+  .join("");
