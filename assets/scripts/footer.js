aabd50b83eba8410ac6eb14570b0a750e7ec48e8
Olga Bulgakova <108359930+OlgaBuLL@users.noreply.github.com>
2024-09-26 19:09:01 +0200

    footer localization (not completed)

--- assets/scripts/footer.js
+++ assets/scripts/footer.js
@@ -6,16 +6,27 @@ import {
 
 const footerDataMap = {
   RU: {
+    logoText: "Онлайн-консультации <br> секс-терапевта <br> Катерины Тартари",
     consent: "Cогласие с",
     consentLink: "Политикой конфиденциальности",
+    subscribe: "Подпишитесь на новости",
+    contacts: "Контакты",
   },
   EN: {
+    logoText:
+      "Online consultations <br> with sex & relationship therapist <br> Katerina Tartary",
     consent: "agreement",
     consentLink: "Privacy policy",
+    subscribe: "Subscribe to the news",
+    contacts: "Contacts",
   },
   FR: {
+    logoText:
+      "Les consultations <br> en ligne de sexothérapeute et consultante en relations <br> Katerina Tartary",
     consent: "Consentement à",
     consentLink: "la politique de confidentialité",
+    subscribe: "S'inscrire aux nouvelles",
+    contacts: "Contacts",
   },
 };
 
@@ -24,8 +35,15 @@ const updateFooterContent = (language) => {
 
   const consent = document.querySelector(".footer__consent-text");
   const consentLink = document.querySelector(".footer__consent-link");
+  const logoText = document.querySelector(".footer__logo-text");
+  const subscribeTitle = document.querySelector(".footer__subscribe-title");
+  const contactsTitle = document.querySelector(".footer__contacts-title");
+
   consent.textContent = content.consent;
   consentLink.textContent = content.consentLink;
+  logoText.innerHTML = content.logoText;
+  subscribeTitle.textContent = content.subscribe;
+  contactsTitle.textContent = content.contacts;
 };
 
 loadLanguage(updateFooterContent);
